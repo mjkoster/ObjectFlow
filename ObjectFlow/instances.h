aab23763b7c790f68921e8c78cf34b43b37f5d27
mjkoster <michaeljohnkoster@gmail.com>
2022-03-13 20:35:43 -0700

    Update instances.h

--- ObjectFlow/instances.h
+++ ObjectFlow/instances.h
@@ -4,7 +4,9 @@ namespace ObjectFlow
     {43000, 0, InputLinkType, 0, linkType, (AnyValueType){.linkType={43001,0}} },
     {43000, 0, CurrentValueType ,0, integerType, (AnyValueType){.integerType=0} },
     {43000, 0, OutputLinkType, 0, linkType, (AnyValueType){.linkType={43002,0}} },
-    {43001, 0, InputValueType ,0, integerType, (AnyValueType){.integerType=0} },
-    {43002, 0, OutputValueType ,0, integerType, (AnyValueType){.integerType=0} },
+
+    {43001, 0, OutputValueType ,0, integerType, (AnyValueType){.integerType=0} },
+    
+    {43002, 0, InputValueType ,0, integerType, (AnyValueType){.integerType=0} },
   };
 }
