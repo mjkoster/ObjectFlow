5e0a4919732e2d09519fd6ce291168fe8551d756
mjkoster <michaeljohnkoster@gmail.com>
2022-03-22 07:54:13 -0700

    cleanup test

--- ObjectFlow/objectflow-test.cpp
+++ ObjectFlow/objectflow-test.cpp
@@ -6,12 +6,8 @@ int main() {
   ObjectList rtu;
   rtu.buildInstances();
   rtu.displayObjects();
-  Object* object1 = rtu.getObjectByID(43001,0);
-  AnyValueType value = object1 -> onInputSync();
-  printf ("%d\n", value.integerType);
-  rtu.displayObjects();
-  Object* object = rtu.getObjectByID(43000,0);
-  object -> syncFromInputLink();
+  printf ("\n");
+  rtu.getObjectByID(43000,0) -> syncFromInputLink();
   rtu.displayObjects();
   return(0);
 };
