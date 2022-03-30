0a6ffb479cd203285406822910602d8b8fb71d49
mjkoster <michaeljohnkoster@gmail.com>
2022-03-29 22:08:43 -0700

    rename flow components, work on builder
    develop the builder logic and workflow

--- Builder/builder.py
+++ Builder/builder.py
@@ -1,23 +1,38 @@
 
 import json
+import yaml
 import glob
+import jsonpointer
+import jsonschema
 
-class Graph:
-  def __init__(self, model):
+class Graph():
+# class Graph(dict):
+  def __init__(self):
     self._graph = {}
-    self.addModel(model)
 
   def addModel(self, model):
     self._merge(model) 
 
-  def _merge(self, graph):
-    self._merge(graph) # recursive merge JSON trees, extend trees and set end values
+  def addFlow(self, flow):
+    self._graph += flow
+
+  def _merge(self, model):
+  # recursive descent merge JSON trees, extend trees and set end values
+  # for each item in the model, check the position in the graph
+  # add to graph if not present until the end value is reached 
+  # set the end value, const overrides default (leave const: )
+    self._merge(model) 
 
   def graph(self):
     return self._graph
 
   def json(self):
-    return( json.dumps(self.graph() ) )
+    # options go here
+    return json.dumps( self.graph() ) 
+
+  def yaml(self):
+    # options go here
+    return yaml.dump( self.graph() ) 
 
 
 class ModelGraph:
@@ -26,32 +41,71 @@ class ModelGraph:
     # read in all of the SDF files in the model directory
     for file in glob.glob( modelPath + "*.sdf.json" ):
       self._modelGraph.addModel( json.loads( open(file,"r").read() ) )
+    for file in glob.glob( modelPath + "*.sdf.yml" ):
+      self._modelGraph.addModel( yaml.load( open(file,"r").read() ) )
   
   def json(self):
-    return( self._modelGraph.json())
+    return self._modelGraph.json()
+
+  def yaml(self):
+    return self._modelGraph.yaml()
 
 
-class InstanceGraph:
-  def __init__(self, modelGraph, instancePath):
+class FlowGraph:
+  def __init__(self, modelGraph, flowPath):
 
-    # InstanceGraph gets filled in with all required items and values defined 
+    # FLowGraph gets filled in with all required items and values defined 
     # the ObjectFlow header can be made from the full InstanceGraph
 
     self._modelGraph = modelGraph
 
-    self._instanceGraph = {}
+    self._flowGraph = Graph()
 
-    for file in glob.glob( instancePath + "*.flow.json" ):
-      self._instanceGraph.addModel( json.loads( open(file,"r").read() ) )
+    for file in glob.glob( flowPath + "*.flow.json" ):
+      self._instanceGraph.addFlow( json.loads( open(file,"r").read() ) )
+    for file in glob.glob( flowPath + "*.flow.yml" ):
+      self._flowGraph.addFlow( yaml.load( open(file,"r").read() ) )
+
+    self._resolveInstances()
+
+
+  def _resolveInstances(self):
+    # process the flow graph to resolve all required items and default values from the model graph
+    # 
+    # for object in flowGraph resolve required items by type in model graph
+    # if no type, add type: <=name>
+    # if required item not present, add it with defaults
+    # if required item is present, check its value
+    # for object values, check required elements and types and fill in missing elements with defaults
+    # assign sequential instance numbers for duplicate instances of Objects and Resources
+    # expand sdfRef pointers as the model is traversed, navigate through JSON pointers
+    #
+    # resolving an SDF instance might be done using back-merge of all the sdfRef entry points from the model
+    # construct an SDF instance graph by expanding items in the flow graph and add it to the model
+    # construct paths to transitive endpoints specified in the flow file e.g.
+    # CurrentValue: 100 becomes CurrentValue: { Value: 100 } which is put into the construct => sdfThing:
+    # CurrentValue: { sdfRef: /#/...CurrentValue Value, ValueType: duckType(100), Value: 100 }
+    # if type is constrained to float, convert integer constant
+    # serialize the instance graph to a resolved flow graph
+    # serialize each object to the header template file
+    #
+    # can resources be added to an object that aren't in the composed model? What would they do?
+    return
+
 
   def json(self):
     return self._instanceGraph.json()
 
-  def objectFlowHeader(self):
-    return self._json2header( self.json() ) # convert the json to a header file
 
-  def _json2header(self, json):
-    self._json = json
+  def yaml(self):
+    return self._instanceGraph.yaml()
+
+
+  def objectFlowHeader(self):
+    return self._header() # convert the resolved instance graph to a header file
+
+
+  def _header(self):
     return
 
 
